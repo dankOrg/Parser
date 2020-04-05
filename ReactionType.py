07fc7380c95f46edb33363d9823db353c81397fa
Anirudh Manohar Chellani <achellan@pop-os.localdomain>
2020-04-05 01:13:32 -0700

    ReactionType

--- ReactionType.py
+++ ReactionType.py
@@ -1,1 +1,13 @@
-class ReactionType():
+
+class ReactionType(Enum):
+    HEART="unicode"
+    SMILE="unicode"
+    OOH="unicode"
+    SAD_TEAR="unicode"
+    ANGRY="unicode"
+    UPVOTE="unicode"
+    DOWNVOTE="unicode"
+
+    def parseReactionFromString(strToParse):
+        # Write code to parse to Reaction From Sring
+
